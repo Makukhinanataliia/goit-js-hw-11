c00d77665955573033c5726fa681f578e9011eb4
Makukhinanataliia <makukhinanataliia@gmail.com>
2024-02-21 19:25:01 +0330

    fix loader

--- src/js/onFormSubmit.js
+++ src/js/onFormSubmit.js
@@ -1,8 +1,7 @@
 import { refs } from './refs';
 import { renderImages } from './renderImages';
 import { getImages } from './getImages';
-import { showLoader } from './loader';
-import { hideLoader } from './loader';
+import { showLoader, hideLoader } from './loader';
 import { showError } from './showError';
 
 export function onFormSubmit(event) {
@@ -10,10 +9,7 @@ export function onFormSubmit(event) {
   showLoader();
   refs.imageContainer.innerHTML = '';
   const userInput = event.target.elements.query.value;
-  getImages(userInput)
-    .then(renderImages)
-    .catch(showError)
-    .finally(hideLoader);
+  getImages(userInput).then(renderImages).catch(showError).finally(hideLoader);
 
   event.target.reset();
 }
