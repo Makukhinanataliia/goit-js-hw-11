1b0ea2b5cd227f861183e7036e9b9faea76bc07e
Makukhinanataliia <makukhinanataliia@gmail.com>
2024-02-21 08:20:02 +0330

    loader

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,9 +1,9 @@
 import { refs } from './refs';
 
-export function loaderOn() {
+export function showLoader() {
   refs.loader.classList.remove('hidden');
 }
 
-export function loaderOff() {
+export function hideLoader() {
   refs.loader.classList.add('hidden');
 }
