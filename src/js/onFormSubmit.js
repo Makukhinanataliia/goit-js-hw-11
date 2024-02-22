a884b77609650ac5534e1182f1ddebcee226e239
Makukhinanataliia <makukhinanataliia@gmail.com>
2024-02-22 14:52:52 +0330

    add improvements

--- src/js/onFormSubmit.js
+++ src/js/onFormSubmit.js
@@ -4,12 +4,20 @@ import { getImages } from './getImages';
 import { showLoader, hideLoader } from './loader';
 import { showError } from './showError';
 
+function isValidInput(input) {
+  return input.trim() !== '';
+}
+
 export function onFormSubmit(event) {
   event.preventDefault();
+  const userInput = event.target.elements.query.value;
+  if (!isValidInput(userInput)) {
+    showError('Invalid query. Please enter a non-empty string.');
+    return;
+  }
   showLoader();
   refs.imageContainer.innerHTML = '';
-  const userInput = event.target.elements.query.value;
-  getImages(userInput).then(renderImages).catch(showError).finally(hideLoader);
 
+  getImages(userInput).then(renderImages).catch(showError).finally(hideLoader);
   event.target.reset();
 }
